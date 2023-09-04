d74b56ea46113e02c2698338c026fafc670a898e
nikemelyanov <emelyanovtime@gmail.com>
2023-09-04 13:49:58 +0300

    post service remake. class structure added

--- src/service/postService.ts
+++ src/service/postService.ts
@@ -1,47 +1,48 @@
 import pool from '../database.js';
 
-export async function createPost(
-  postTitle: string,
-  postBody: string,
-  authorId: number,
-  author: string,
-  createdDate: string
-) {
-  try {
-    const result = await pool.query(
-      'INSERT INTO posts(title, body, user_id, author, date) VALUES ($1, $2, $3, $4, $5)',
-      [postTitle, postBody, authorId, author, createdDate]
-    );
-  } catch (err) {
-    console.error(err);
+class postServiceClass {
+  async createPost(
+    postTitle: string,
+    postBody: string,
+    authorId: number,
+    author: string,
+    createdDate: string
+  ) {
+    try {
+      const result = await pool.query(
+        'INSERT INTO posts(title, body, user_id, author, date) VALUES ($1, $2, $3, $4, $5)',
+        [postTitle, postBody, authorId, author, createdDate]
+      );
+    } catch (err) {
+      console.error(err);
+    }
   }
-}
-export async function deletePost(postId: number) {
-  try {
-    const result = pool.query('DELETE FROM posts WHERE id = $1', [postId]);
-  } catch (err) {
-    console.error(err);
+  async deletePost(postId: number) {
+    try {
+      const result = pool.query('DELETE FROM posts WHERE id = $1', [postId]);
+    } catch (err) {
+      console.error(err);
+    }
   }
-}
-export async function getAllPosts() {
-  try {
-    const result = await pool.query('SELECT * FROM posts');
-    return result.rows
-  } catch (err) {
-    console.error(err);
+  async getAllPosts() {
+    try {
+      const result = await pool.query('SELECT * FROM posts');
+      return result.rows;
+    } catch (err) {
+      console.error(err);
+    }
   }
-}
-export async function getOnePost(postTitle: string) {
-  try {
-    const result = await pool.query('SELECT * FROM posts WHERE title = $1', [
-      postTitle,
-    ]);
-    const post = result.rows[0];
-    return post;
-  } catch (err) {
-    console.error(err);
+  async getOnePost(postTitle: string) {
+    try {
+      const result = await pool.query('SELECT * FROM posts WHERE title = $1', [
+        postTitle,
+      ]);
+      const post = result.rows[0];
+      return post;
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
-
-
+export const postService = new postServiceClass();
