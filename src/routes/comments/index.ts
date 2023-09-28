61601a7b21eb35d7837336bb823e03bf930d3399
nikemelyanov <emelyanovtime@gmail.com>
2023-09-28 15:43:23 +0300

    f/postcomments update

--- src/routes/comments/index.ts
+++ src/routes/comments/index.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import { CommentsController } from '../../controllers/comments/commentsController.js';
 
 router.post('/getComments', CommentsController.getComments)
+router.post('/createComment', CommentsController.createComment)
 
 export default router;
