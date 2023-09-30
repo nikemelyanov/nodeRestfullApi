2b47436fc4a36f064d596f5018dd986e0d3e1de4
nikemelyanov <emelyanovtime@gmail.com>
2023-09-30 14:38:34 +0300

    delete app directory

--- app/routes/users/auth/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { authController } from '../../../controllers/auth/authController.js';
-const router = express.Router();
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-export default router;
