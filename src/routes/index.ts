d03bbbd2f87f575f3fbd0c6b1f145ce1a198d43c
nikemelyanov <emelyanovtime@gmail.com>
2023-12-19 15:48:18 +0300

    new api router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+
+import authRouter from "../routes/users/auth";
+import postRouter from "../routes/posts";
+import commentsRouter from "../routes/comments";
+import likesRouter from "../routes/likes";
+import usersRouter from "../routes/users/update";
+
 const router = express.Router();
 
-router.get('/', (req: any, res: any) => {
-  res.json({ hello: 'hello retwitzzz' });
-});
+router.use("/auth", authRouter);
+router.use("/posts", postRouter);
+router.use("/comments", commentsRouter);
+router.use("/likes", likesRouter);
+router.use("/users", usersRouter);
 
 export default router;
