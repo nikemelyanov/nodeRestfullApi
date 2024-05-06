85b586f18a9e824e5103fd2dc00c21bc783889c8
Your Name <emelyanovtime@gmail.com>
2024-05-06 14:14:50 +0300

    mini fix, delete comments

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,5 @@
 import pg from 'pg';
 
-// const pool = new pg.Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'node_db',
-//   password: 'root',
-//   port: 5432,
-// });
-
 const pool = new pg.Pool({
   user: 'root',
   host: 'localhost',
